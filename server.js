132faef793c82336d49803e439cb2261112bdf49
ahsanrasheed786 <rasheedahsan786@gmail.com>
2024-04-28 12:50:07 +0500

    live

--- server.js
+++ server.js
@@ -3,21 +3,36 @@ const mongoose = require('mongoose');
 const contactController = require('./controllers/contactController');
 const scheduleController = require('./controllers/scheduleController');
 const userController = require('./controllers/userController');
+const dotenv=require('dotenv')
 const cors=require('cors');
 const app = express();
-const port = process.env.PORT || 3000;
+
+dotenv.config();
+
+const  port =process.env.PORT
+const mongodb =process.env.MONGODB
+const dbUrl = process.env.DBNAME
+const frontendUrl = process.env.FRONTENDURL
 
 app.use(express.json());
 app.use(cors(
     {
-      origin: '*',
+      origin: frontendUrl,
       credentials: true,
     }
 ));
-mongoose.connect('mongodb://localhost:27017/your-database-name', {
-  // useNewUrlParser: true,
-  // useUnifiedTopology: true,
-});
+
+
+const connectDB = async () => {
+  try {
+   const conn = await mongoose.connect(mongodb);
+    console.log(`MongoDB connected successfully with Database : ${dbUrl} and with host: ${conn.connection.host}`);
+  } catch (err) {
+    console.error(err.message);
+    process.exit(1);
+  }
+}
+connectDB()
 
 app.post('/api/contact', contactController.createContact);
 app.get('/api/contact', contactController.getContactSubmissions);
@@ -27,6 +42,6 @@ app.get('/api/schedule', scheduleController.getScheduledServices);
 app.post('/api/login', userController.login);
 app.post('/api/signup', userController.signup);
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+app.listen(port , () => {
+  console.log(`Server is running on port ${ port }`);
 });
